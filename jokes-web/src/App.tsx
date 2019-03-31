b9a51321ba97193a7e56b5e11625f80df03c7098
Sam Curry <sam.curry@readify.net>
2019-03-31 09:31:54 +0800

    add create-react-app --typescript starter + signalr

---
+++ jokes-web/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+
+interface AppState {
+  jokes: string[];
+}
+
+class App extends Component<{}, AppState> {
+
+  public state: AppState = {
+    jokes: []
+  }
+
+  constructor() {
+    super({});
+    this.getJokes();
+  }
+
+  private getJokes() {
+    const connection = new HubConnectionBuilder().withUrl('http://localhost:5000').build();
+    connection.on("jokes", (joke: string) => {
+      this.state.jokes.push(joke);
+      this.setState(this.state);
+    })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.jokes.map(joke => <li>{joke}</li>)}
+      </ul>
+    );
+  }
+}
+
+export default App;
