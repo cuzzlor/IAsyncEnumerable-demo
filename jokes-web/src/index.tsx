81f8bf985900ec1857a923b4469b50cb52db0ed1
Sam Curry <sam.curry@readify.net>
2019-04-01 18:37:55 +0800

    slow it down

--- jokes-web/src/index.tsx
+++ jokes-web/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App jokes={{delay: 3000, maxListSize: 5}} />, document.getElementById('root'));
+ReactDOM.render(<App jokes={{delay: 5000, maxListSize: 5}} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
